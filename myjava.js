ef9322f72cb810d4afa03005f59481823383f2d9
Whitney Clegg <whitney.clegg@journalism.cuny.edu>
2016-02-17 23:06:27 -0500

    first commit

---
+++ myjava.js
@@ -0,0 +1,23 @@
+var singleRide;
+var monthlyCard;
+
+
+function calculate(){
+	singleRide = document.getElementById("subway").value;
+	monthlyCard = singleRide * 4;
+	alert("You ride "+
+	monthlyCard+
+	" times per month");
+	shouldYou();
+}
+	
+	function shouldYou(){
+		if (monthlyCard > 46){
+			alert("Purchase a monthly pass! You're spending too much money.");
+		}
+		else { 
+			alert("Keep doing what you're doing. No need to buy a monthly pass.")
+	}
+
+
+}
